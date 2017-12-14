ed3d8eafa191a69f0e3b163c5fd2ce2365683b80
Nazmus Saadat <nuhin13@Nazmuss-Mac.local>
2017-12-14 14:14:56 +0600

    Initial Commit

---
+++ Ios_Networking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ios_Networking
+//
+//  Created by Nazmus Saadat on 14/12/17.
+//  Copyright © 2017 Nazmus Saadat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
